6e632caa92d63bba03ae9bfdd3a1e259dc0ee843
Carolina Ferreiro Belenguer <88061350+cferreirobelenguer@users.noreply.github.com>
2023-08-09 22:45:09 +0200

    onClick

--- src/component/Home.jsx
+++ src/component/Home.jsx
@@ -1,6 +1,10 @@
 import React from 'react';
 import '../component/Home.css';
 
+const handleClick = (path) => {
+    window.location.href= path;
+}
+
 const Home = () => {
     return (
         <div className="home-container">
@@ -14,19 +18,19 @@ const Home = () => {
                         <p className="home-container-buttons-section-titleButton">Ubuntu Desktop</p>
                         <p className="home-container-buttons-section-subtitleButton">Download Ubuntu desktop and replace your current operating system or run alongside it.</p>
                     </div>
-                    <button className="home-container-buttons-section-button">Download v 20.04</button>
-                    <a href="" className="link-color">Download earlier version</a>
+                    <button className="home-container-buttons-section-button" onClick={() => handleClick('https://ubuntu.com/download/desktop/thank-you?version=22.04.2&architecture=amd64')} >Download v 20.04</button>
+                    <a href="https://ubuntu.com/download/desktop/thank-you?version=22.04.2&architecture=amd64" className="link-color">Download earlier version</a>
                 </div>
                 <div className="home-container-buttonsContainer">
                     <div className="home-container-buttons-section">
                         <p className="home-container-buttons-section-titleButton">Ubuntu Server</p>
                         <p className="home-container-buttons-section-subtitleButton">Download Ubuntu desktop and replace your current operating system or run alongside it.</p>
                     </div>
-                    <button className="home-container-buttons-section-button">Get Ubuntu Server</button>
+                    <button className="home-container-buttons-section-button" onClick={() => handleClick('https://ubuntu.com/download/server')}>Get Ubuntu Server</button>
                     <div className="home-container-buttons-types link-color">
-                        <a href="">Mac and Windows</a>
-                        <a href="">ARM</a>
-                        <a href="">IBM</a>
+                        <a href="https://ubuntu.com/download/server#downloads">Mac and Windows</a>
+                        <a href="https://ubuntu.com/download/server/arm">ARM</a>
+                        <a href="https://ubuntu.com/download/server/s390x">IBM</a>
                     </div>
                 </div>
             </section>
